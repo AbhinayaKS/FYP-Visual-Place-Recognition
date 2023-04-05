9bca5ad18a388bcde722e78651fcdcdbcac5be80
Abhinaya Srinivasan <abhinaya.kms@gmail.com>
2023-04-05 21:18:07 +0800

    generate VLAD Encoding at the endpoint

--- Application/Flask Server/server.py
+++ Application/Flask Server/server.py
@@ -6,15 +6,30 @@ import h5py
 import faiss
 import numpy as np
 from netvlad import NetVLAD
-from torchsummary import summary
+from torch.autograd import Variable
 import torch.nn.functional as F
 from os.path import join
+import torchvision.transforms as transforms
+from PIL import Image
+import json
 
 from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+transform_pipeline = transforms.Compose([
+        transforms.ToTensor(),
+        normalize,
+])
+
+class Flatten(nn.Module):
+    def forward(self, input):
+        return input.view(input.size(0), -1)
 
 class L2Norm(nn.Module):
     def __init__(self, dim=1):
@@ -27,28 +42,39 @@ class L2Norm(nn.Module):
 @app.before_first_request
 def setupModel():
     print("Setting up model")
-    # Load model here
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("Device: ", device)
+    torch.manual_seed(42)
 
     encoder = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1)
-    # capture only feature part and remove last relu and maxpool
     layers = list(encoder.features.children())[:-2]
     for l in layers[:-5]: 
         for p in l.parameters():
             p.requires_grad = False
     encoder = nn.Sequential(*layers)
+    global model
+    model = nn.Module()
+    model.add_module('encoder', encoder)
     net_vlad = NetVLAD(num_clusters=64, dim=512)
-    encoder.add_module('pool', net_vlad)
-    encoder.to(device)
+    model.add_module('pool', net_vlad)
+
     resume_ckpt = join('data/vgg16_checkpoint', 'checkpoints', 'model_best.pth.tar')
     checkpoint = torch.load(resume_ckpt, map_location=lambda storage, loc: storage)
-    encoder.load_state_dict(checkpoint['state_dict'])
+    model.load_state_dict(checkpoint['state_dict'])
+    model = model.to(device)
+    model.eval()
     print('Loaded model from {}'.format(resume_ckpt))
 
 class SemanticSeg(Resource):
     def get(self):
-        return {'hello': 'world'}
+        with torch.no_grad():
+            img = Image.open('data/boost.jpg')
+            img = transform_pipeline(img)
+            img = img.unsqueeze(0)  
+            img = Variable(img)
+            img = img.to(device)
+            image_encoding = model.encoder(img)
+            vlad_encoding = model.pool(image_encoding)
+
+        return {'hello': json.dumps(vlad_encoding.tolist())}
 
 class getNearestNeighbours(Resource):
     def get(self):
